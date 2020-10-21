43e6371bb533fd8529b447b2c80130e22469c99e
Nikolaev Viktor <maclauddd@gmail.com>
2020-10-21 21:07:26 +0300

    extracted modules from index file

---
+++ src/game.js
@@ -0,0 +1,75 @@
+import { clearScreen } from "./screen";
+import { renderDoodler, jump } from "./doodler";
+import { createPlatforms, movePlatforms } from "./platforms";
+import { control } from "./control";
+
+export function start(state) {
+  Object.keys(state.timers).forEach((name) =>
+    clearInterval(state.timers[name])
+  );
+
+  clearScreen(state.screen.element);
+
+  state.platforms = createPlatforms({
+    screen: state.screen.element,
+    platformCount: state.platformCount,
+    leftSpace: state.doodler.leftSpace,
+  });
+
+  renderDoodler({
+    screen: state.screen.element,
+    doodler: state.doodler.element,
+    leftSpace: state.doodler.leftSpace,
+    bottomSpace: state.doodler.bottomSpace,
+  });
+
+  movePlatforms(state.platforms, state.doodler.bottomSpace);
+
+  jump(state);
+
+  state.timers.platforms = setInterval(() => {
+    movePlatforms(state.platforms, state.doodler.bottomSpace);
+  }, state.frameMs);
+}
+
+export function initGame({
+  startPoint,
+  screen,
+  buttonStart,
+  doodler,
+  isJumping = true,
+  platformCount = 5,
+  frameMs = 16,
+  score = 0,
+  platforms = [],
+}) {
+  const initialState = {
+    screen: {
+      startPoint,
+      element: screen,
+    },
+    doodler: {
+      element: doodler,
+      leftSpace: startPoint,
+      bottomSpace: startPoint,
+      isJumping,
+    },
+    timers: {
+      up: 0,
+      left: 0,
+      right: 0,
+      platforms: 0,
+      fallDown: 0,
+    },
+    score,
+    platforms,
+    frameMs,
+    platformCount,
+  };
+
+  buttonStart.addEventListener("click", () => {
+    start(initialState);
+  });
+
+  document.addEventListener("keyup", (e) => control(e, initialState));
+}
---
+++ src/platforms.js
@@ -0,0 +1,57 @@
+export function createPlatform(
+  screen,
+  bottomPosition,
+  leftPosition = Math.random() * 315
+) {
+  const bottom = bottomPosition;
+  const left = leftPosition;
+  const element = document.createElement("div");
+
+  element.classList.add("platform");
+  element.style.bottom = `${bottom}px`;
+  element.style.left = `${left}px`;
+
+  screen.appendChild(element);
+
+  return {
+    bottom,
+    left,
+    element,
+  };
+}
+
+export function createPlatforms({ screen, platformCount, leftSpace }) {
+  let result = [];
+
+  for (let i = 0; i < platformCount; i++) {
+    let platformGap = 600 / platformCount;
+    let newPlatformBottom = 100 + i * platformGap;
+
+    let left;
+
+    if (i === 0) {
+      left = leftSpace;
+    }
+
+    let newPlatform = createPlatform(screen, newPlatformBottom, left);
+
+    result.push(newPlatform);
+  }
+
+  return result;
+}
+
+export function movePlatforms(platforms, bottomSpace) {
+  if (bottomSpace > 150) {
+    platforms.forEach((platform) => {
+      platform.bottom -= 2;
+      platform.element.style.bottom = `${platform.bottom}px`;
+      platform.element.style.left = `${platform.left}px`;
+
+      if (platform.bottom <= 5) {
+        platform.bottom = 600;
+        platform.left = Math.random() * 315;
+      }
+    });
+  }
+}
