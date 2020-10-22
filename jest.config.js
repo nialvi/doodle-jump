23e1ae163cf1919d057fafb64bfc18f9e9a133f7
Nikolaev Viktor <maclauddd@gmail.com>
2020-10-22 21:42:18 +0300

    added watch and debug mode. added threshold

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  collectCoverageFrom: ["**/src/**/*.js"],
+  coverageThreshold: {
+    global: {
+      statements: 1,
+      branches: 0,
+      functions: 1,
+      lines: 1,
+    },
+  },
 };
